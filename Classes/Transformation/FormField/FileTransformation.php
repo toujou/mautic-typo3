e6ba7a08063fac45552ca3b2ada685551baff9ba
Tobias Wojtylak <twojtylak@dfau.de>
2024-02-27 11:27:20 +0100

    feat: add FileTransformation class to create uploads

---
+++ Classes/Transformation/FormField/FileTransformation.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Bitmotion\Mautic\Transformation\FormField;
+
+class FileTransformation extends AbstractFormFieldTransformation
+{
+    protected $type = 'file';
+}
